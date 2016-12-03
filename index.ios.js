44dec22e71e631d55184a5c9b8057a7314651749
Kevin Amiranoff <kamiranoff@gmail.com>
2016-12-03 18:33:01 +0100

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+// IOS :)
+import React from 'react';
+import { View, Text, AppRegistry } from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>
+      Hello
+    </Text>
+  </View>
+);
+
+// Render to the device
+AppRegistry.registerComponent('Mythology', () => App);
